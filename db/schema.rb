3553fb91df3c7b6166395381c16714383812dffb
Uliana Dzoba <ulianadzoba@gmail.com>
2020-03-27 16:41:16 +0200

    Refactor application to be polymorphic

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_084947) do
+ActiveRecord::Schema.define(version: 2020_03_27_123451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shouts", force: :cascade do |t|
-    t.string "body", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content_type"
+    t.integer "content_id"
+    t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
     t.index ["user_id"], name: "index_shouts_on_user_id"
+  end
+
+  create_table "text_shouts", force: :cascade do |t|
+    t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
