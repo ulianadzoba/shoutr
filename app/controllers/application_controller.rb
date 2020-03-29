f4946a681c19b29e61d2769557cc62be0b91e561
Uliana Dzoba <ulianadzoba@gmail.com>
2020-03-29 15:56:53 +0300

    Fix user page when not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+  protect_from_forgery with: :exception
+
+  def current_user
+    super || Guest.new
+  end
 end
+
+
