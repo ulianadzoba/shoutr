9dff345622770262b5f87223544c7ac39e4d4bf8
Uliana Dzoba <ulianadzoba@gmail.com>
2020-03-30 20:34:33 +0300

    Extract Timeline and Dashboard class

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,6 @@
 class DashboardsController < ApplicationController
-    def show
-        @shout = Shout.new
-        @shouts = current_user.shouts
-    end
+  def show
+    @dashboard = Dashboard.new(current_user)
+  end
     
 end
