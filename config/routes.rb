e6b66a251cd8b2a787b2516e1d2122cb72678ec2
Uliana Dzoba <ulianadzoba@gmail.com>
2020-03-30 18:52:21 +0300

    Updated the controller to be open for extension

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
 
   root to: "homes#show"
 
-  resources :shouts, only: [:create, :show] do 
+  post "text_shouts" => "shouts#create", defaults: { content_type: TextShout }
+  post "photo_shouts" => "shouts#create", defaults: { content_type: PhotoShout }
+
+  resources :shouts, only: [:show] do 
     member do
       post "like" => "likes#create"
       delete "unlike" => "likes#destroy"
